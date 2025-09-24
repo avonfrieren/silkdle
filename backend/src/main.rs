3ce5a84b5b0898554a117545398a638d4d643070
PaleNoVaFrieren <angeloportelli05@gmail.com>
2025-09-24 17:47:25 +0200

    basic rust server
    - static site
    - rust backend working
    - basic template

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,20 @@
-use axum::{routing::get, Router};
+use axum::Router;
+use tower_http::services::ServeDir;
 use std::net::SocketAddr;
-
-async fn hello() -> &'static str {
-    "Hello from Rust backend!"
-}
 
 #[tokio::main]
 async fn main() {
-    // Créer les routes
-    let app = Router::new().route("/", get(hello));
+    // router
+    let app = Router::new().nest_service(
+        "/",
+        ServeDir::new("../static").append_index_html_on_directories(true),
+    );
 
-    // Adresse d’écoute
+    // adresse d'ecoute
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("🚀 Server running at http://{}", addr);
 
-    // Lancement du serveur (avec hyper)
+    // lancement du server avec hyper
     axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
         .await
         .unwrap();
