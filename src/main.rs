af729b7f5e33b35c496db22295468f6574cf865a
PaleNoVaFrieren <angeloportelli05@gmail.com>
2025-09-29 01:27:32 +0200

    comparing and displaying data working

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,25 @@ use axum::{ routing::get, Router};
 use tower_http::services::ServeDir;
 use std::net::SocketAddr;
 use askama_axum::Template;
+use std::fmt;
 
 // pages
 #[derive(Template)]
 #[template(path = "homepage.html")]
-struct HomepageTemplate<'a> {
-    test: &'a str,
+struct HomepageTemplate {
+    test: &'static str,
+    results_zone: Vec<CompareResult>,
 }
 
 async fn homepage() -> impl axum::response::IntoResponse {
-    HomepageTemplate { 
-        test: "silkdle"
+    let z1 = Zone { name: "Moss Grotto", size: 19, acte: 1, bosses: 4, station: true };
+    let z2 = Zone { name: "The Marrow", size: 18, acte: 1, bosses: 2, station: true };
+
+    let results = z1.compare(&z2);
+
+    HomepageTemplate {
+        test: "silkdle",
+        results_zone: results,
     }
 }
 
@@ -38,69 +46,81 @@ struct Zone<'a> {
     station: bool, // is there a station in the zone
 }
 
+enum CompareResult {
+    Smaller,
+    Greater,
+    Equal,
+    NotCorrect
+}
+
+impl fmt::Display for CompareResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CompareResult::Smaller => write!(f, "Smaller"),
+            CompareResult::Greater => write!(f, "Greater"),
+            CompareResult::Equal => write!(f, "Equal"),
+            CompareResult::NotCorrect => write!(f, "NotCorrect"),
+        }
+    }
+}
+
+fn compare_values<T: Ord>(a: &T, b: &T) -> CompareResult {
+    if a < b {
+        CompareResult::Smaller
+    } else if a > b {
+        CompareResult::Greater
+    } else {
+        CompareResult::Equal
+    }
+}
+
+fn compare_str(a: &str, b: &str) -> CompareResult {
+    if a == b {
+        CompareResult::Equal
+    } else {
+        CompareResult::NotCorrect
+    }
+}
+
+fn compare_bool(a: &bool, b: &bool) -> CompareResult {
+    if a == b {
+        CompareResult::Equal
+    } else {
+        CompareResult::NotCorrect
+    }
+}
+
 impl Zone<'_> {
-    fn compare(&self, other: &Zone) {
-        if self.name == other.name {
-            println!("correct : name");
-        } else {
-            println!("not correct : name")
-        }
+    fn compare(&self, other: &Zone) -> Vec<CompareResult> {
+        let mut results = Vec::new();
 
-        if self.size <= other.size {
-            println!("smaller : size");
-        } else if self.size >= other.size {
-            println!("greater : size")
-        } else {
-            println!("correct : size");
-        }
+        results.push(compare_str(&self.name, &other.name));
+        results.push(compare_values(&self.size, &other.size));
+        results.push(compare_values(&self.acte, &other.acte));
+        results.push(compare_values(&self.bosses, &other.bosses));
+        results.push(compare_bool(&self.station, &other.station));
 
-        if self.acte < other.acte {
-            println!("smaller : acte");
-        } else if self.acte > other.acte {
-            println!("greater : acte")
-        } else {
-            println!("correct : acte");
-        }
-
-        if self.bosses <= other.bosses {
-            println!("smaller : bosses");
-        } else if self.bosses >= other.bosses {
-            println!("greater : bosses")
-        } else {
-            println!("correct : bosses");
-        }
-        
-        if self.station == other.station {
-            println!("correct : station");
-        } else {
-            println!("not correct : station")
-        }
+        results
     }
 }
 
 #[tokio::main]
 async fn main() {
-    // test
-    let z1 = Zone { name: "Moss Grotto", size: 19, acte: 1, bosses: 4, station: true };
-    let z2 = Zone { name: "The Marrow", size: 18, acte: 1, bosses: 2, station: true };
-
-    z1.compare(&z2);
-
     // router
-    // let static_files = axum::Router::new().nest_service(
-    //     "/static",
-    //     ServeDir::new("static")
-    // );
+    let static_files = axum::Router::new().nest_service(
+        "/static",
+        ServeDir::new("static")
+    );
 
-    // let app = Router::new()
-    //     .route("/", get(homepage))
-    //     .route("/login", get(login))
-    //     .merge(static_files);
+    let app = Router::new()
+        .route("/", get(homepage))
+        .route("/login", get(login))
+        .merge(static_files);
 
-    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    // println!("🚀 Server running at http://{}", addr);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("🚀 Server running at http://{}", addr);
 
-    // axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
-    //     .await
-    //     .unwrap();
+    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
+        .await
+        .unwrap();
 }
