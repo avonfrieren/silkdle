c63a646b35698e25e414d60ade72e4702f37f3de
PaleNoVaFrieren <angeloportelli05@gmail.com>
2025-09-28 23:05:04 +0200

    bad, slow and not finished but started main logic

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use tower_http::services::ServeDir;
 use std::net::SocketAddr;
 use askama_axum::Template;
 
-#[derive(Template)] // code automatiquemetn generer permettant a la struct d'etre rendu en html
-#[template(path = "homepage.html")] // indique que le template se trouve a la racine du dossier templates lui meme par defaut a la racine
+// pages
+#[derive(Template)]
+#[template(path = "homepage.html")]
 struct HomepageTemplate<'a> {
     test: &'a str,
 }
 
 async fn homepage() -> impl axum::response::IntoResponse {
-    // instantiation, injection d'une variable et renvoi du template a afficher
     HomepageTemplate { 
         test: "silkdle"
     }
@@ -28,27 +28,79 @@ async fn login() -> impl axum::response::IntoResponse {
     }
 }
 
+// core logic
+#[derive(Debug)]
+struct Zone<'a> {
+    name: &'a str, // name on the map
+    size: u8, // numbers of rooms (for now)
+    acte: u8, // 1, 2 or 3: is it accessible normally ? (the intended way)
+    bosses: u8, // numbers of bosses, not elite mobs
+    station: bool, // is there a station in the zone
+}
+
+impl Zone<'_> {
+    fn compare(&self, other: &Zone) {
+        if self.name == other.name {
+            println!("correct : name");
+        } else {
+            println!("not correct : name")
+        }
+
+        if self.size <= other.size {
+            println!("smaller : size");
+        } else if self.size >= other.size {
+            println!("greater : size")
+        } else {
+            println!("correct : size");
+        }
+
+        if self.acte < other.acte {
+            println!("smaller : acte");
+        } else if self.acte > other.acte {
+            println!("greater : acte")
+        } else {
+            println!("correct : acte");
+        }
+
+        if self.bosses <= other.bosses {
+            println!("smaller : bosses");
+        } else if self.bosses >= other.bosses {
+            println!("greater : bosses")
+        } else {
+            println!("correct : bosses");
+        }
+        
+        if self.station == other.station {
+            println!("correct : station");
+        } else {
+            println!("not correct : station")
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
+    // test
+    let z1 = Zone { name: "Moss Grotto", size: 19, acte: 1, bosses: 4, station: true };
+    let z2 = Zone { name: "The Marrow", size: 18, acte: 1, bosses: 2, station: true };
+
+    z1.compare(&z2);
+
     // router
-    // dis au routeur que l'url "/static" correspond au dossier statique sur le disque pour qu'il sache ou allez chercher les assets
-    let static_files = axum::Router::new().nest_service(
-        "/static",
-        ServeDir::new("static")
-    );
+    // let static_files = axum::Router::new().nest_service(
+    //     "/static",
+    //     ServeDir::new("static")
+    // );
 
-    // instantiation router, declaration des routes, fusion des fichiers statique
-    let app = Router::new()
-        .route("/", get(homepage))
-        .route("/login", get(login))
-        .merge(static_files);
+    // let app = Router::new()
+    //     .route("/", get(homepage))
+    //     .route("/login", get(login))
+    //     .merge(static_files);
 
-    // adresse d'ecoute
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("🚀 Server running at http://{}", addr);
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // println!("🚀 Server running at http://{}", addr);
 
-    // lancement du server avec hyper
-    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
-        .await
-        .unwrap();
+    // axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
+    //     .await
+    //     .unwrap();
 }
